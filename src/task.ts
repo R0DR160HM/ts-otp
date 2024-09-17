aeea964b2e91eade6739cad8cf49d15ac67f7625
Rodrigo Heinzen <rodrigo.h.m.112200@gmail.com>
2024-09-17 08:06:12 -0300

    ♻️ Recycling threads

--- src/task.ts
+++ src/task.ts
@@ -1,17 +1,17 @@
 import { Result } from "./result";
 
-const WORKER_SCRIPT = `
-    const base = self;
+const sleepingWorkers = new Array<Worker>();
 
+const WORKER_SCRIPT = `
     onmessage = function(msg) {
         const data = msg.data;
         const args = data.args;
         const command = data.command;
         
-        base.job = args[0];
+        self.job = args[0];
         
         if (command !== "run") {
-            base.postMessage({
+            self.postMessage({
                 status: "error",
                 error: {
                     message: "Invalid command!"
@@ -19,13 +19,13 @@ const WORKER_SCRIPT = `
             });
         }
 
-        const func = new Function("return " + base.job.doFunction)();
-        func(base.job.args)
+        const func = new Function("return " + self.job.operation)();
+        func(self.job.args)
             .then(function (value) {
-                base.postMessage({ status: "ok", value })
+                self.postMessage({ status: "ok", value })
             })
             .catch(function (error) {
-                base.postMessage({ status: "error", error })
+                self.postMessage({ status: "error", error })
             });
     }
 `;
@@ -53,33 +53,40 @@ function async<T, K extends object>(callback: (args: K) => Promise<T>, context: 
 }
 
 function asyncRaw<T, K extends object>(callback: (args: K) => Promise<T>, context: K): Promise<T> {
-    const blobURL = URL.createObjectURL(new Blob(
-        [WORKER_SCRIPT],
-        { type: 'application/javascript' }
-    ));
-    const worker = new Worker(blobURL);
+    let blobURL: string;
+    if (!sleepingWorkers.length) {
+        blobURL = URL.createObjectURL(new Blob(
+            [WORKER_SCRIPT],
+            { type: 'application/javascript' }
+        ));
+        sleepingWorkers.push(new Worker(blobURL));
+    }
+    const worker = sleepingWorkers.shift()!;
 
     return new Promise((resolve, reject) => {
         worker.addEventListener('error', err => {
             reject(err);
             worker.terminate();
-            URL.revokeObjectURL(blobURL);
+            if (blobURL) {
+                URL.revokeObjectURL(blobURL);
+            }
         });
         worker.addEventListener('message', ev => {
+            if (!sleepingWorkers.includes(worker)) {
+                sleepingWorkers.push(worker);
+            }
             if (ev.data.status === 'ok') {
                 resolve(ev.data.value);
             } else {
                 reject(ev.data.error);
             }
-            worker.terminate();
-            URL.revokeObjectURL(blobURL);
         });
 
         worker.postMessage({
             command: 'run',
             args: [{
                 args: context,
-                doFunction: callback.toString()
+                operation: callback.toString()
             }]
         });
     })
