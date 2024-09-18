e5b0dc49b844004204d2fe6ff9c650aed3a0d54d
Rodrigo Heinzen de Moraes <rodrigo.h.m.112200@gmail.com>
2024-09-18 20:41:20 -0300

    🧬 Making processes sufficiently complete

--- src/process.ts
+++ src/process.ts
@@ -1,4 +1,9 @@
+import { Result } from './result';
+
 const WORKER_SCRIPT = `
+    var Θ__otp_pid;
+    var func;
+
     onmessage = function(msg) {
         const data = msg.data;
         const args = data.args;
@@ -7,8 +12,12 @@ const WORKER_SCRIPT = `
         self.job = args[0];
         
         switch (command) {
+            case "setup":
+                Θ__otp_pid = self.job.args;
+                func = new Function("return " + self.job.operation)();
+                break;
+
             case "run":
-                const func = new Function("return " + self.job.operation)();
                 func(self.job.args)
                     .then(function (value) {
                         self.postMessage({ status: "ok", value });
@@ -19,6 +28,7 @@ const WORKER_SCRIPT = `
                 break;
 
             case "kill":
+                func = null;
                 self.postMessage({ status: "done" });
                 break;
 
@@ -36,24 +46,67 @@ const WORKER_SCRIPT = `
 
 const subjectPool = new Array<Worker>();
 const alocatedThreads = new Map<Pid, Worker>();
+const nameRegistry = new Map<string, Pid>();
+const activeTimers = new Array<Timer>();
 
-// abstract class CallError {
-//     public static CalleeDown = class extends CallError {
-//         constructor(public readonly reason: unknown) {
-//             super();
-//         }
-//     }
-//     public static CallTimeout = class extends CallError { }
-// }
+/**
+ * To make the library compatible with Node
+ */
+class FakeWorker<T, K> {
+    private listeners: any[] = [];
 
-// abstract class Cancelled {
-//     public static TimerNotFound = class extends Cancelled { }
-//     public static Cancelled = class extends Cancelled {
-//         constructor(public readonly timeRemaining: number) {
-//             super();
-//         }
-//     }
-// }
+    constructor(private implementation: (that: T) => Promise<K>) {}
+
+    public addEventListener(_event: 'message', listener: any) {
+        this.listeners.push(listener);
+    }
+
+    public postMessage(message: { command: 'run'; args: { args: T }[] }) {
+        this.implementation(message.args[0].args)
+            .then((value) => {
+                for (const listener of this.listeners) {
+                    listener({ data: { status: 'ok', value } });
+                }
+            })
+            .catch((error) => {
+                for (const listener of this.listeners) {
+                    listener({ data: { status: 'error', error } });
+                }
+            });
+    }
+}
+
+abstract class CallError {
+    public static CalleeDown = class extends CallError {
+        constructor(public readonly reason: unknown) {
+            super();
+        }
+    };
+    public static CallTimeout = class extends CallError {};
+}
+
+class Timer {
+    public readonly createdAt = new Date().getTime();
+    constructor(public readonly time: number, action: () => void) {
+        activeTimers.push(this);
+        setTimeout(() => {
+            const index = activeTimers.indexOf(this);
+            if (index >= 0) {
+                action();
+                activeTimers.splice(index, 1);
+            }
+        }, time);
+    }
+}
+
+abstract class Cancelled {
+    public static TimerNotFound = class extends Cancelled {};
+    public static Cancelled = class extends Cancelled {
+        constructor(public readonly timeRemaining: number) {
+            super();
+        }
+    };
+}
 
 // abstract class ExitReason {
 //     public static Normal = class extends ExitReason { }
@@ -71,6 +124,9 @@ class Pid {
     constructor() {
         this.id = ++Pid.lastId;
     }
+    public get subject(): Worker | null {
+        return alocatedThreads.get(this) || null;
+    }
 }
 
 // class ExitMessage {
@@ -87,27 +143,40 @@ class Pid {
 //     ) { }
 // }
 
-function start<T, K>(implementation: (that: K) => Promise<T>, context: K): Pid {
-    if (!window?.URL?.createObjectURL || !window?.Worker?.prototype?.postMessage) {
-        implementation(context);
-        return new Pid();
+function start<T, K>(implementation: (that: K) => Promise<T>): Pid {
+    if (
+        !window?.URL?.createObjectURL ||
+        !window?.Worker?.prototype?.postMessage
+    ) {
+        return _fakeStart(implementation);
     }
     try {
-        return _start(implementation, context);
+        return _start(implementation);
     } catch (err) {
-        implementation(context);
-        return new Pid();
+        return _fakeStart(implementation);
     }
+}
+
+function _fakeStart<T, K>(implementation: (that: K) => Promise<T>): Pid {
+    const pid = new Pid();
+    const worker = new FakeWorker(implementation);
+    alocatedThreads.set(pid, worker as any);
+    return pid;
 }
 
 /**
  * @todo implement link
  */
-function _start<T, K>(implementation: (that: K) => Promise<T>, context: K, /* link: boolean */): Pid {
+function _start<T, K>(
+    implementation: (that: K) => Promise<T>
+    /* link: boolean */
+): Pid {
     let blobURL: string;
     let subject = subjectPool.shift()!;
     if (!subject) {
-        blobURL = URL.createObjectURL(new Blob([WORKER_SCRIPT], { type: 'application/javascript' }));
+        blobURL = URL.createObjectURL(
+            new Blob([WORKER_SCRIPT], { type: 'application/javascript' })
+        );
         subject = new Worker(blobURL);
     }
 
@@ -128,11 +197,13 @@ function _start<T, K>(implementation: (that: K) => Promise<T>, context: K, /* li
     alocatedThreads.set(pid, subject);
 
     subject.postMessage({
-        command: 'run',
-        args: [{
-            args: context,
-            operation: implementation.toString()
-        }]
+        command: 'setup',
+        args: [
+            {
+                args: pid.id,
+                operation: implementation.toString()
+            }
+        ]
     });
 
     return pid;
@@ -141,7 +212,7 @@ function _start<T, K>(implementation: (that: K) => Promise<T>, context: K, /* li
 function kill(pid: Pid): void {
     const subject = alocatedThreads.get(pid);
     if (subject) {
-        subject.onmessage = ev => {
+        subject.onmessage = (ev) => {
             if (ev.data?.status === 'done') {
                 alocatedThreads.delete(pid);
                 if (!subjectPool.includes(subject)) {
@@ -154,15 +225,145 @@ function kill(pid: Pid): void {
     }
 }
 
+function register(pid: Pid, name: string): Result<null, null> {
+    if (!alocatedThreads.has(pid)) {
+        return new Result.Error(null);
+    }
+    if (nameRegistry.has(name)) {
+        return new Result.Error(null);
+    }
+    for (const registeredPid of nameRegistry.values()) {
+        if (registeredPid === pid) {
+            return new Result.Error(null);
+        }
+    }
+    if (!name || name === 'undefined') {
+        return new Result.Error(null);
+    }
+    nameRegistry.set(name, pid);
+    return new Result.Ok(null);
+}
+
+function unregister(name: string): Result<null, null> {
+    if (nameRegistry.delete(name)) {
+        return new Result.Ok(null);
+    }
+    return new Result.Error(null);
+}
+
+function named(name: string): Result<Pid, null> {
+    const pid = nameRegistry.get(name);
+    if (pid) {
+        return new Result.Error(null);
+    }
+    return new Result.Ok(pid);
+}
+
+function isAlive(a: Pid): boolean {
+    return alocatedThreads.has(a);
+}
+
+function send<T>(subject: Worker, message: T): void {
+    subject.postMessage({
+        command: 'run',
+        args: [
+            {
+                args: message
+            }
+        ]
+    });
+}
+
+function sendAfter<T>(subject: Worker, delay: number, message: T): Timer {
+    return new Timer(delay, () => {
+        subject.postMessage({
+            command: 'run',
+            args: [
+                {
+                    args: message
+                }
+            ]
+        });
+    });
+}
+
+function cancelTimer(timer: Timer) {
+    const index = activeTimers.indexOf(timer);
+    if (index >= 0) {
+        activeTimers.splice(index, 1);
+        const timePassed = new Date().getTime() - timer.createdAt;
+        const timeRemaining = timer.time - timePassed;
+        return new Cancelled.Cancelled(timeRemaining);
+    }
+    return new Cancelled.TimerNotFound();
+}
+
+/**
+ * Intentionally diverges from specification,
+ * it will likely change in the future
+ */
+function call<T, K>(
+    subject: Worker,
+    makeRequest: T,
+    timeout: number
+): Promise<K> {
+    return new Promise((resolve, reject) => {
+        subject.addEventListener('message', (ev) => {
+            if (ev.data.status === 'ok') {
+                resolve(ev.data.value);
+            } else if (ev.data.status === 'error') {
+                reject(ev.data.error);
+            }
+        });
+
+        send(subject, makeRequest);
+
+        setTimeout(() => {
+            throw new Error('Process crashed!');
+        }, timeout);
+    });
+}
+
+function tryCall<T, K>(
+    subject: Worker,
+    makeRequest: T,
+    timeout: number
+): Promise<Result<K, CallError>> {
+    return new Promise((resolve) => {
+        subject.addEventListener('message', (ev) => {
+            if (ev.data.status === 'ok') {
+                resolve(new Result.Ok(ev.data.value));
+            } else {
+                resolve(new Result.Error(new CallError.CalleeDown(ev.data)));
+            }
+        });
+
+        send(subject, makeRequest);
+
+        setTimeout(() => {
+            resolve(new Result.Error(new CallError.CallTimeout()));
+        }, timeout);
+    });
+}
 
 export const process = {
-    // CallError,
-    // Cancelled,
+    CallError,
+    Timer,
+    Cancelled,
     // ExitReason,
     // ExitMessage,
     Pid,
     // ProcessDown,
 
     start,
-    kill
+    kill,
+    register,
+    unregister,
+    named,
+    isAlive,
+    send,
+    sendAfter,
+    cancelTimer,
+    call,
+    tryCall
 };
